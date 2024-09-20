5af4c72a5b2289c81f3b7e8f0f71dad2dfc4a792
JuliCode07 <Julicode07@gmail.com>
2024-09-19 19:27:02 -0500

    commented localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Quiz from "./Quiz";
-import ResetLocalStorage from "./resetLocalStorage";
 
 const MainRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Quiz />} />
-        <Route path="/reset" element={<ResetLocalStorage />} />
       </Routes>
     </Router>
   );
